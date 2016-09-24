c9521fff9e632cbd2f22519eec1a693815f84fca
Anton <Anton1400>
2016-09-25 02:30:26 +0300

    v

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,39 @@
+import hashlib
+import os
+import sys
+
+files = {}
+if len(sys.argv) == 1:
+    print('Вы не указали адресс папки')
+    sys.exit(1)
+os.chdir(sys.argv[1])
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def get_hash_md5(filename):
+    with open(filename, 'rb') as f:
+        m = hashlib.md5()
+        while True:
+            data = f.read(8192)
+            if not data:
+                break
+            m.update(data)
+        return m.hexdigest()
 
 
-if __name__ == '__main__':
-    pass
+def check_copies():
+    for i in os.walk(os.getcwd(), topdown=True, onerror=None, followlinks=False):
+        os.chdir(i[0])
+        for k in i[2]:
+            Hash = get_hash_md5(k)
+            if not (Hash in files):
+                files[Hash] = [i[0]+'/'+k]
+            else:
+                files[Hash].append(i[0]+'/'+k)
+
+
+check_copies()
+for i in files:
+    if len(files[i]) > 1:
+        print("Для файла", files[i][0], "найдены слудующие копии:")
+        for k in files[i][1:]:
+            print(k)
